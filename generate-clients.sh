4eb6c0050608587ee888af3656f816c5cd2ee05c
Andreas Frankenberger <andreasfrankenberger@gmail.com>
2020-07-21 10:02:18 +0200

    Publish typescript-angular client on release

---
+++ generate-clients.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+rm -rf clients
+mkdir clients
+
+API_PATH="/local/openapi.yaml"
+IMAGE="openapitools/openapi-generator-cli:v4.3.1"
+COMMON_PARAMS="--api-package api --model-package model --git-host github.com --git-repo-id pricemonitor-clients --git-user-id Patagona"
+
+# Generating scala clients is temporarily deactivated since they don't have priority
+'docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH ${COMMON_PARAMS} \
+      -g scala-akka \
+      -o /local/clients/akka
+
+docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH \
+      -g scala-sttp \
+      -o /local/clients/scala-sttp
+
+docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH \
+      -g scala-gatling \
+      -o /local/clients/scala-gatling
+
+docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH \
+      -g scalaz \
+      -o /local/clients/scalaz
+
+docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH \
+      -g java \
+      -o /local/clients/java '
+
+docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE generate \
+      -i $API_PATH \
+      -g typescript-angular \
+      -o /local/clients/typescript-angular ${COMMON_PARAMS} \
+      --additional-properties=npmName=@Patagona/pricemonitor-typescript-angular \
+      --additional-properties=ngVersion=10.0.0 \
+      --additional-properties=npmRepository=https://npm.pkg.github.com
+
+# The generator inserts crappy typescript boundaries which only work for angualr 9. We need to update them.
+ANGULAR_PACKAGE_JSON=clients/typescript-angular/package.json
+sed -i 's/\"typescript\": \">=3.6.0 <3.8.0\"/\"typescript\": \">=3.9.2 <4.0.0\"/g' ${ANGULAR_PACKAGE_JSON}
+# We want to publish the client as "typescript-angular". Therefore we have to add this repository entry.
+# See https://docs.npmjs.com/files/package.json#repository
+jq -M '. +{"repository": {"type" : "git","url" : "ssh://git@github.com/Patagona/pricemonitor-clients.git","directory": "clients/typescript-angular"}}' ${ANGULAR_PACKAGE_JSON} > ${ANGULAR_PACKAGE_JSON}.tmp
+mv ${ANGULAR_PACKAGE_JSON}.tmp ${ANGULAR_PACKAGE_JSON}
