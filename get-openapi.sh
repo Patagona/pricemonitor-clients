5067fe25298b22cfe2f9fcb06b6e9bb8146b1fcb
Erwyn van der Meer <e.vandermeer@omniaretail.com>
2024-04-03 16:39:04 +0200

    Get OAS file from patagona-api-docs repo

--- get-openapi.sh
+++ get-openapi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-release=$( curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/Patagona/Patagona-API/releases/tags/${API_VERSION} )
-json=$( echo $release | jq '.assets[] | select(.name|test("openapi-internal.yaml"))')
-url=$( echo $json | jq -r '.url')
+
+# Wait for other workflows to create the combined OpenAPI specification file in the the patagona-api-docs repo.
+sleep 60
+
+# Download the combined OpenAPI specification file.
+branch_name=staging-${API_VERSION}
+url=https://api.github.com/repos/Patagona/patagona-api-docs/contents/assets-joined/internal-with-scheduler-openapi.yaml?ref=${branch_name}
 echo "Downloading file $url"
-curl -LJ --header "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/octet-stream' $url > openapi-internal.yaml
+
+curl -s -H "Authorization: token $GITHUB_TOKEN" $url > openapi-internal.yaml
