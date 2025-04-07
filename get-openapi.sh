410e1114a90e9d42380d2b7b28620132d2a4c76c
Julian Bieber <j.bieber@omniaretail.com>
2025-04-07 16:02:52 +0200

    get verbose logs from curl when retrieving openapi spec for generation

--- get-openapi.sh
+++ get-openapi.sh
@@ -8,4 +8,4 @@ branch_name=release-general-${API_VERSION}
 url=https://api.github.com/repos/Patagona/patagona-api-docs/contents/assets-joined/internal-with-scheduler-openapi.yaml?ref=${branch_name}
 echo "Downloading file $url"
 
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" $url > openapi-internal.yaml
+curl -v -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" $url -o openapi-internal.yaml
