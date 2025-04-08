24d318b29e9cef16e4395a14798df693d9569453
julianbieber <julian.bieber.1992@gmail.com>
2025-04-08 10:33:12 +0200

    remove curl --fail-with-body parameter since the curl version on the jenkins is too old (#51)

--- get-openapi.sh
+++ get-openapi.sh
@@ -8,4 +8,4 @@ branch_name=release-general-${API_VERSION}
 url=https://api.github.com/repos/Patagona/patagona-api-docs/contents/internal-api/omnia-internal.yaml?ref=${branch_name}
 echo "Downloading file $url"
 
-curl -v --fail-with-body -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" $url -o openapi-internal.yaml
+curl -v -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" $url -o openapi-internal.yaml
